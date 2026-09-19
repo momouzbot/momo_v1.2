e15b181f003bf1ae1f42247fb10bf9d50b1c8548
momouzbot <momouzbot@gmail.com>
2026-09-19 15:52:50 +0500

    Add PremiumSubscriber and PremiumSubscriptionPayment models

--- mce_momo_bot/app/models/__init__.py
+++ mce_momo_bot/app/models/__init__.py
@@ -12,6 +12,7 @@ from app.models.custom import CustomButton, CustomCommand
 from app.models.game import GameInstance, GamePlayer, GameSettings
 from app.models.module import Module
 from app.models.payment import HostingPayment, Payment, TariffUpgrade
+from app.models.premium_subscription import PremiumSubscriber, PremiumSubscriptionPayment
 from app.models.sub_admin import BotSubAdmin
 from app.models.tariff import Tariff
 from app.models.user import User
@@ -26,6 +27,8 @@ __all__ = [
     "FeatureUsageLog",
     "BroadcastLog",
     "BotSubAdmin",
+    "PremiumSubscriber",
+    "PremiumSubscriptionPayment",
     "Tariff",
     "HostingPayment",
     "TariffUpgrade",
